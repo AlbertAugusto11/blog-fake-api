c20edcb0434916e70570134d98f43e63a30e6e78
Albert Augusto <167814598+AlbertAugusto11@users.noreply.github.com>
2024-10-23 17:01:14 -0300

    FINALIZADO
      const [mode,setMode] = useState(() => {
        const savedMode = localStorage.getItem('modeDark');
        return savedMode ? JSON.parse(savedMode) : false;
      });
    
    Principal ALTERAÇÃO

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,20 @@ import "./app.scss"
 
 
 function App() {
-  const localMode = localStorage.getItem("modeDark")
-  console.log(localMode)
-  
-  const [mode,setMode] = useState(localMode == true ? localMode : false)
+
+  const [mode,setMode] = useState(() => {
+    const savedMode = localStorage.getItem('modeDark');
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
   
   const modeDarkF = () =>{
     setMode(!mode)
-    localStorage.setItem("modeDark", !mode)
   }
+
+  useEffect(() =>{
+    localStorage.setItem("modeDark", mode)
+  },[mode])
 
   return(
     <div className={mode == false ? "div__1" : "div__2"}>
